fb605a52820f4d4fb7bb5e0584ebf1fe7f3a1105
Omkar Yadav <me@f22.dev>
2021-10-29 04:05:30 +0400

    Update ignore plugin params for Next 11 (#31)

--- example/next.config.js
+++ example/next.config.js
@@ -2,7 +2,9 @@ module.exports = {
   webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
       // Ensures no server modules are included on the client.
-      config.plugins.push(new webpack.IgnorePlugin(/lib\/server/));
+      config.plugins.push(new webpack.IgnorePlugin({
+        resourceRegExp: /lib\/server/
+      }));
     }
 
     return config;
