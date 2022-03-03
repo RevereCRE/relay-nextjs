5d285756c39e3b7731a3b464f9cb5063ab264f2a
gio-work <95727886+gio-work@users.noreply.github.com>
2022-03-02 16:26:24 -0800

    Pass error to ErrorComponent  (#49)
    * pass error to erorr boundary
    
    * implement feedback to consolidate hasError
    
    * remove console log
    
    * Initiality false state
    
    * refactor to use this.state.error directly

--- src/wired/error_boundry.tsx
+++ src/wired/error_boundry.tsx
@@ -1,32 +1,37 @@
 import Error, { ErrorProps } from 'next/error';
 import React, { Component, PropsWithChildren } from 'react';
+import { NextPageContext } from 'next';
 
 export type WiredErrorBoundaryProps = PropsWithChildren<{
-  ErrorComponent?: React.ComponentType<ErrorProps>;
+  ErrorComponent?: React.ComponentType<
+    ErrorProps & { error: NextPageContext['err'] }
+  >;
 }>;
 
 interface WiredErrorBoundaryState {
-  hasError: boolean;
+  error?: NextPageContext['err'] | false;
 }
 
 export class WiredErrorBoundary extends Component<
   WiredErrorBoundaryProps,
   WiredErrorBoundaryState
 > {
-  static getDerivedStateFromError(): WiredErrorBoundaryState {
-    return { hasError: true };
+  static getDerivedStateFromError(
+    error: NextPageContext['err']
+  ): WiredErrorBoundaryState {
+    return { error };
   }
 
-  state = { hasError: false };
+  state: { error? : NextPageContext['err'] | false } = { error: false }
 
   render() {
     const ErrorComponent = this.props.ErrorComponent;
 
-    if (this.state.hasError) {
+    if (this.state.error) {
       return ErrorComponent ? (
-        <ErrorComponent statusCode={500} />
+        <ErrorComponent statusCode={500} error={this.state.error} />
       ) : (
-        <Error statusCode={500} />
+        <Error statusCode={500} error={this.state.error} />
       );
     } else {
       return this.props.children;
